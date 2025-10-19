13ac9aed2983d4d68d1082cda158211d37c80a3a
angelofernandezlipan <a.fernandez18@ufromail.cl>
2025-10-18 23:19:23 -0300

    Código compilable con su Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Iniciando prueba de CarroCompra");
+        CarroCompra miCompra = new CarroCompra();
+        miCompra.mostrarTotal();
+
+        System.out.println("Prueba finalizada.");
+    }
+}
