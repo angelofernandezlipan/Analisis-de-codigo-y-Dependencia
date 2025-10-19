12b5469a112857bf11486f81187b4af803843209
angelofernandezlipan <a.fernandez18@ufromail.cl>
2025-10-18 23:14:20 -0300

    Commit con código generado de Visual Paradigm

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,36 @@
+public class Calculadora {
+
+	private int n1;
+	private int n2;
+
+	/**
+	 * 
+	 * @param num1
+	 * @param num2
+	 */
+	public Calculadora(int num1, int num2) {
+		// TODO - implement Calculadora.Calculadora
+		throw new UnsupportedOperationException();
+	}
+
+	public int multiplicar() {
+		// TODO - implement Calculadora.multiplicar
+		throw new UnsupportedOperationException();
+	}
+
+	public int sumar() {
+		// TODO - implement Calculadora.sumar
+		throw new UnsupportedOperationException();
+	}
+
+	public void setN1() {
+		// TODO - implement Calculadora.setN1
+		throw new UnsupportedOperationException();
+	}
+
+	public void setN2() {
+		// TODO - implement Calculadora.setN2
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/main/java/CarroCompra.java
@@ -0,0 +1,29 @@
+public class CarroCompra {
+
+	private int[][] productos;
+
+	public CarroCompra() {
+
+	}
+
+	private int calcularTotal() {
+		// TODO - implement Calculadora2.calcularTotal
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param cant
+	 * @param precio
+	 */
+	private int subTotal(int cant, int precio) {
+		// TODO - implement Calculadora2.subTotal
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarTotal() {
+		// TODO - implement Calculadora2.mostrarTotal
+		throw new UnsupportedOperationException();
+	}
+
+}
